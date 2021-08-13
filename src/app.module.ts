95e82e48da69daebd2546e12b50ed9fa3d3f11ca
juand <juandavidds@.es>
2021-08-13 12:11:25 -0500

    api json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './healt/healt.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'client'),
+      }),
+    
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
